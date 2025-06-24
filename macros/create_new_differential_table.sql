dbfaf36b8e3f393c38d5ffa86c7226d8618620af
Kyle Ratliff <kyle.ratliff@sailpoint.com>
2025-06-24 16:55:46 +0000

    added the create_new_differential_table SQL file

--- macros/create_new_differential_table.sql
+++ macros/create_new_differential_table.sql
@@ -1,11 +1,38 @@
- 
-{% macro create_new_diff_table(target_table, target_schema) %}
+ {% macro create_new_differential_table(target_table, project=target.project, dataset=target.dataset, dry_run=True) %}
+    
+    {% set get_create_commands_query %}
+    SELECT 
+        REPLACE(
+            REPLACE(
+                REPLACE(
+                    REPLACE(
+                        REPLACE(
+                            REPLACE(
+                                REPLACE(ddl,
+                             'BOOL', 'STRING')
+                            , 'FLOAT64', 'STRING')
+                        ,'INT64','STRING')
+                    ,'{{ target_table ~ '_non_string_schema'}}','{{ target_table  ~ '_new_differential'}}')
+                ,'DATETIME','STRING')
+            ,'DATE','STRING')
+        ,'TIMESTAMP','STRING') AS new_ddl
+    FROM `{{ project }}.region-us.INFORMATION_SCHEMA.TABLES`
+    WHERE table_name = '{{ target_table ~ '_non_string_schema'}}' 
+    AND upper(table_schema) = upper('{{ dataset }}')
+    {% endset %}
 
-  SELECT 
-    REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(REPLACE(ddl, 'BOOL', 'STRING'), 'FLOAT64', 'STRING'),'INT64','STRING'),'' || target_table || '_non_string_schema','' || target_table || '_new_differential'),'DATETIME','STRING'),'DATE','STRING'),'TIMESTAMP','STRING') AS new_ddl
-    ,ROW_NUMBER() OVER ()
- FROM `sandbox-444621.region-us.INFORMATION_SCHEMA.TABLES`
-WHERE table_name = target_table ||'_non_string_schema'
-AND table_schema = target_schema
+    {{ log('Print Create DDL command:' + get_create_commands_query, info=True)}}
 
-{% endmacro %}
+    {{ log('\nGenerating create queries...\n', info=True) }}
+    {% set create_queries = run_query(get_create_commands_query).columns[0].values() %}
+
+    {% for query in create_queries %}
+        {% if dry_run %}
+            {{ log(query, info=True) }}
+        {% else %}
+            {{ log('Dropping object with command: ' ~ query, info=True) }}
+            {% do run_query(query) %} 
+        {% endif %}       
+    {% endfor %}
+    
+{% endmacro %} 
--- models/diff_merge_model.sql
+++
@@ -1,2 +0,0 @@
-
-CALL `sandbox-444621.admin.mergetable_from_new_differential_complete_process`('stg_salesforce', 'salesforce_account', 'id');
